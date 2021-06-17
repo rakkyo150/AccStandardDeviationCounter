19b3d1be4dcef08453985f164ea0d8abc9d4e6f6
rakkyo150 <yoshimura150@hotmail.co.jp>
2021-06-18 00:06:39 +0900

    プロジェクト ファイルを追加します。

---
+++ AccStandardDeviationCounter/Logger.cs
@@ -0,0 +1,10 @@
+﻿using IPALogger = IPA.Logging.Logger;
+
+
+namespace AccStandardDeviationCounter
+{
+    internal static class Logger
+    {
+        internal static IPALogger log { get; set; }
+    }
+}
